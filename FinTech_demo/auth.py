36dcfc48a5aae981a033c3bf6a5051a8858ea483
Bigggi <68317236+MRAmateurH@users.noreply.github.com>
2022-06-12 00:33:42 +0800

    update

--- FinTech_demo/auth.py
+++ FinTech_demo/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
+
 @auth.route('/login')
 def login():
     return render_template('login.html')
@@ -10,3 +11,8 @@ def login():
 @auth.route('/register')
 def register():
     return render_template('register.html')
+
+
+@auth.route('/password')
+def password():
+    return render_template('password.html')
--- FinTech_demo/views.py
+++ FinTech_demo/views.py
@@ -2,6 +2,12 @@ from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
+
+@views.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+
 @views.route('/')
 def home():
-    return render_template('dashboard.html')
+    return render_template('home.html')
